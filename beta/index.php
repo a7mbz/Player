1912bc8504074bd73760cf7cf4bd83965dbb55d3
a7mbz <bouziane.ahs@gmail.com>
2026-08-31 17:29:58 +0100

    Update index.php

--- beta/index.php
+++ beta/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YouTube Player - Search & Suggestions</title>
+    <title>YouTube premium</title>
 
 <!-- ========================================================================= -->
 <!-- 1. STYLES & LAYOUT (CSS)                                                 -->
